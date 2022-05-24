44619f5cbe6243709170e3932ff0c21ab0db507d
Vladyslava <vladatsyupyak@gmail.com>
2022-05-24 14:11:19 +0300

    fix: only 1 sign can be put

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ let buttonsContainer = document.querySelector('.buttons-wrapper')
 let operators = document.querySelectorAll('.sign');
 let number = '';
 let sign = '';
+let canPutSign = true;
 let strOfNumberBeforeSign = ''
 
 function createStrBeforeCount (number) {
-
     strOfNumberBeforeSign = strOfNumberBeforeSign + number;
-    console.log(strOfNumberBeforeSign)
     return strOfNumberBeforeSign
 }
 
 function convertStrToSign (eventTarget) {
-    if (eventTarget.innerHTML === '+') {
-        number = ''
-        createStrBeforeCount('+');
-    }
+   if (canPutSign) {
+       number = ''
+       sign = eventTarget.innerHTML
+       createStrBeforeCount(' '+ sign + ' ');
+       canPutSign = false
+   }
 }
-
-
-
+function getNumValue(eventTarget) {
+    number = eventTarget.target.innerHTML
+    canPutSign = true
+}
 
 buttonsContainer.addEventListener('click', function (e) {
     if(e.target.closest( '.number')) {
-        console.log('----' + e.target.innerHTML)
-         number = e.target.innerHTML
+        getNumValue(e);
     }
 
     if(e.target.closest('.sign')) {
